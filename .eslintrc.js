1aaac1b1b1c34472ae0b3340139d2bfcc83be8fc
robBowes <32183505+robBowes@users.noreply.github.com>
2018-03-09 10:55:28 -0500

    Add linting file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {"ecmaVersion": 6},
+    "env":{
+        "node": true,
+    },
+    "rules": {
+        "linebreak-style":["off"],
+        "require-jsdoc": ["off"],
+    }
+};
