b8290595043e0d2719ed11ff04fe194b334766ad
robBowes <32183505+robBowes@users.noreply.github.com>
2018-03-11 20:09:35 -0400

    Minor Updates

--- public/3d_terrain_generation/sketch.js
+++ public/3d_terrain_generation/sketch.js
@@ -1,13 +1,13 @@
 let cols;
 let rows;
-let w =1200;
+let w = 1200;
 let h = 1200;
-let resolution = 20;
+let resolution = 30;
 let terrain = [];
 let flying = 0;
 
 
-setup = () =>{
+setup = () => {
     const canvas = createCanvas(600, 600, WEBGL);
     canvas.parent('sketch-holder');
     cols = h / resolution;
@@ -24,22 +24,23 @@ draw = () => {
     for (let i = 0; i < rows; i++) {
         let xoff = flying;
         for (let j = 0; j < cols; j++) {
-            terrain[i][j] = map(noise(yoff, xoff), 0, 2, -100, 200);
+            terrain[i][j] = (map(noise(yoff, xoff), 0, 2, -100, 300));
             xoff += 0.1;
         }
         yoff += 0.1;
     }
     background(0);
-    translate(0, 30);
+    translate(0, 30, -50);
     stroke(255);
-    rotateX(PI/3);
-    fill(200, 200, 200, 100);
-    translate(-w/2, -h/1.5);
-    for (let j = 0; j < rows-1; j++) {
+    rotateX(PI / 3);
+    noFill();
+    // fill(200, 200, 200);
+    translate(-w / 2, -h / 1.5);
+    for (let j = 0; j < rows - 1; j++) {
         beginShape(TRIANGLE_STRIP);
         for (let i = 0; i < cols; i++) {
             vertex(i * resolution, j * resolution, terrain[i][j]);
-            vertex(i * resolution, (j+1) * resolution, terrain[i][j+1]);
+            vertex(i * resolution, (j + 1) * resolution, terrain[i][j + 1]);
         }
         endShape();
     }
